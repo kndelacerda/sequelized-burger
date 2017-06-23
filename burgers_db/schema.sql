1ebbe3df1a312ceff30b56244c99e4fe01068efb
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-23 14:49:54 -0500

    Adjusted to fit to jawsDB

--- burgers_db/schema.sql
+++ burgers_db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS burgers_sequelized_db;
 
 USE burgers_sequelized_db;
 
-CREATE TABLE burgers_db.burgers (
+CREATE TABLE burgers_sequelized_db.burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(30) NOT NULL,
   devoured TINYINT(1),
--- models/burger.js
+++ models/burger.js
@@ -1,32 +1,58 @@
-var orm = require('../config/orm');
+'use strict';
 
+module.exports = function(sequelize, DataTypes) {
 
-var burger = {
-    table: 'burgers',
+    var Burgers = sequelize.define('Burgers', {
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        devoured: {
+            allowNull: false,
+            type: DataTypes.BOOLEAN
+        }
+    }, {
+        underscored: true,
+        classMethods: {
+            associate: function(models) {
+                Burgers.belongsTo(models.Customers, { foreignKey: 'customer_id' });
+            }
+        }
+    });
 
-    allBurgers: function(cb) {
-        orm.all(this.table, function(res) {
-            cb(res);
-        });
-    },
-
-    saveBurger: function(cols, vals, cb) {
-        orm.create(this.table, cols, vals, function(res) {
-            cb(res);
-        });
-    },
-
-    devourBurger: function(objColVals, condition, cb) {
-        orm.update(this.table, objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-
-    trashBurger: function(condition, cb) {
-        orm.delete(this.table, condition, function(res) {
-            cb(res);
-        });
-    }
+    return Burgers;
 };
 
-module.exports = burger;
+
+// var orm = require('../config/orm');
+
+
+// var burger = {
+//     table: 'burgers',
+
+//     allBurgers: function(cb) {
+//         orm.all(this.table, function(res) {
+//             cb(res);
+//         });
+//     },
+
+//     saveBurger: function(cols, vals, cb) {
+//         orm.create(this.table, cols, vals, function(res) {
+//             cb(res);
+//         });
+//     },
+
+//     devourBurger: function(objColVals, condition, cb) {
+//         orm.update(this.table, objColVals, condition, function(res) {
+//             cb(res);
+//         });
+//     },
+
+//     trashBurger: function(condition, cb) {
+//         orm.delete(this.table, condition, function(res) {
+//             cb(res);
+//         });
+//     }
+// };
+
+// module.exports = burger;
