9a578c4d7ce6b30b2450c3a637982edc903587ae
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-23 16:46:01 -0500

    new

--- burgers_sequelized_db/seeds.sql
+++
@@ -1,7 +0,0 @@
-USE burgers_sequelized_db;
-
-INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("veggieburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("mushroomburger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("mysteryburger", false);
